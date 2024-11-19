bea7d7545837e6db2f85367da9a4344db9cf624f
Lisa-CtrlShiftZ <heesterslisa@gmail.com>
2024-11-19 14:49:27 +0100

    test

--- shop.js
+++ shop.js
@@ -196,6 +196,66 @@ function removeFromSaved(childId) {
     }
 }
 
+/* events fired on the draggable target */
+source.addEventListener("drag", (event) => {
+    console.log("dragging");
+  });
+  
+  source.addEventListener("dragstart", (event) => {
+    // store a ref. on the dragged elem
+    dragged = event.target;
+    // make it half transparent
+    event.target.classList.add("dragging");
+  });
+  
+  source.addEventListener("dragend", (event) => {
+    // reset the transparency
+    event.target.classList.remove("dragging");
+  });
+  
+  /* events fired on the drop targets */
+  const target = document.getElementById("drop-target");
+  target.addEventListener(
+    "dragover",
+    (event) => {
+      // prevent default to allow drop
+      event.preventDefault();
+    },
+    false,
+  );
+  
+  target.addEventListener("dragenter", (event) => {
+    // highlight potential drop target when the draggable element enters it
+    if (event.target.classList.contains("dropzone")) {
+      event.target.classList.add("dragover");
+    }
+  });
+  
+  target.addEventListener("dragleave", (event) => {
+    // reset background of potential drop target when the draggable element leaves it
+    if (event.target.classList.contains("dropzone")) {
+      event.target.classList.remove("dragover");
+    }
+  });
+  
+  target.addEventListener("drop", (event) => {
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+    console.log(dragged)
+    const draggedElement = document.getElementById(dragged);
+    const toy = draggedElement.value;
+    addToy(toy)
+    // move dragged element to the selected drop target
+    if (event.target.classList.contains("dropzone")) {
+      event.target.classList.remove("dragover");
+      event.target.appendChild(dragged);
+    }
+  });
+  function addToy(toy){
+  
+  }
+  
+
 
 
 fetchdata(); // haalt uit json database
